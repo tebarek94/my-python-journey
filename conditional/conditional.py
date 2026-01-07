cb3265d187fa363550ad8f8963f7da7e29a6cffc
tebarek94 <tebarekabas4@gmail.com>
2026-01-07 12:12:05 +0300

    code check and add valiadte function

--- conditional/conditional.py
+++ conditional/conditional.py
@@ -1,22 +1,23 @@
 calculation_to_units = 24
 name_of_unit = "hours"
-calculated_value = calculation_to_units * calculation_to_units
 
 def days_to_unit(number_of_days):
     if number_of_days > 0:
         return f"{number_of_days} days are {number_of_days * calculation_to_units} {name_of_unit}"
     elif number_of_days == 0:
-        return "You entered 0 days, which equals 0 seconds"
+        return "You entered 0 days, which equals 0 hours"
     else:
         return "You have entered a negative number, so it cannot be converted"
 
-try:
-    user_input = input("Enter number of days you want to calculate: ")
-    if(user_input.isdigit()):
-        user_input_number= int(user_input)
-        calculated_value = days_to_unit(user_input)
+def validate_and_execute():
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+        calculated_value = days_to_unit(user_input_number)  # FIXED
         print(calculated_value)
     else:
-        print("Please enter a number")
-except ValueError:
-    print("Invalid input. Please enter a valid integer.")
+        print("Please enter a valid number")
+
+user_input = input("Enter number of days you want to calculate: ")
+validate_and_execute()
+
+
