f988ede964beaf6eb5e75858770cf8d2862bd5f5
tebarek94 <tebarekabas4@gmail.com>
2026-01-07 11:57:55 +0300

    Add basics conditional in python

--- basics/userinput/userinput.py
+++ basics/userinput/userinput.py
@@ -1,9 +1,9 @@
 
-calculation_to_unit=24*60*60
-name_of_unit='seconds'
-
-def days_t0_unites(numbers_of_days,custom_message):
-    print(f'{numbers_of_days} days are  {numbers_of_days * calculation_to_unit} {name_of_unit}')
-    print(f'{numbers_of_days} days are  {custom_message}')
-user_input=int(input('Enter number of days you want to calculate : '))
-days_t0_unites(user_input,'Awesome!')
+# calculation_to_unit=24*60*60
+# name_of_unit='seconds'
+#
+# def days_t0_unites(numbers_of_days,custom_message):
+#     print(f'{numbers_of_days} days are  {numbers_of_days * calculation_to_unit} {name_of_unit}')
+#     print(f'{numbers_of_days} days are  {custom_message}')
+# user_input=int(input('Enter number of days you want to calculate : '))
+# days_t0_unites(user_input,'Awesome!')
---
+++ conditional/conditional.py
@@ -0,0 +1,18 @@
+calculation_to_units = 24
+name_of_unit = "hours"
+calculated_value = calculation_to_units * calculation_to_units
+
+def days_to_unit(number_of_days):
+    if number_of_days > 0:
+        return f"{number_of_days} days are {number_of_days * calculation_to_units} {name_of_unit}"
+    elif number_of_days == 0:
+        return "You entered 0 days, which equals 0 seconds"
+    else:
+        return "You have entered a negative number, so it cannot be converted"
+
+try:
+    user_input = int(input("Enter number of days you want to calculate: "))
+    calculated_value = days_to_unit(user_input)
+    print(calculated_value)
+except ValueError:
+    print("Invalid input. Please enter a valid integer.")
